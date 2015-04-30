63e0fe470a58ef12eb0a8da6e873d1c6bc54c38a
Jennifer Le <jle034@hammer.cs.ucr.edu>
2015-04-30 15:55:51 -0700

    fixed some bugs and updated so that ls.cpp keeps track of files/directories being passed in

--- src/ls.cpp
+++ src/ls.cpp
@@ -4,77 +4,75 @@
 //#include <sys/wait.h>
 #include <stdlib.h>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 
 int main(int argc, char* argv[]) {
 	
-	// single flags
-	string a = "-a";
-	string l = "-l";
-	string R = "-R";
-	
-	// flags starting with a
-	string al = "-al";
-	string aR = "-aR";
-	string alR = "-alR";
-	string aRl = "-aRl";
-
-	// flags starting with l
-	string la = "-la";
-	string lR = "-lR";
-	string laR = "-laR";
-	string lRa = "-lRa";
-
-	// flags starting with R
-	string Ra = "-Ra";
-	string Rl = "-Rl";
-	string Ral = "-Ral";
-	string Rla = "-Rla";
-
 	// initialize flags to zero
 	// if flag is still zero after checking, 
 	// then flag is not found in user input
 	int aFlag = 0;
 	int lFlag = 0;
 	int RFlag = 0;
+	vector<string> filesVec;
 
-	// for loop checks for flags
-	for(int i = 0; i < argc; i++) {
-		if(argv[i][0] == '-') {
+	// for loop checks for flags and stores files and directories
+	// begin at 1, so search doesn't include initial bin/ls argument
+	// if flag is found, set appropriate flag to 1
+	// if file or directory is found, push back onto filesVec
+	for(int i = 1; i < argc; i++) {
+		
+		string currArg(argv[i]);
+
+		// if currArg begins with '-', it is considered a flag
+		// do the following
+		if(currArg.at(0) == '-') {
 			
-			string temp(argv[i]);
-
-			if(temp.size() == 1) {
-				cout << "Error: Invalid flag." << endl;
+			if(currArg.length() == 1) {
+				cout << "Error: One or more of these flags is invalid." << endl;
 				exit(1);
 			}
 			
-			for(unsigned j = 1; j < temp.length();  j++) {
+			for(unsigned j = 1; j < currArg.length();  j++) {
 				
-				if(temp.at(j) == 'a') {
+				if(currArg.at(j) == 'a') {
 					aFlag = 1;
 				}
-				else if(temp.at(j) == 'l') {
+				else if(currArg.at(j) == 'l') {
 					lFlag = 1;
 				}
-				else if(temp.at(j) == 'R') {
+				else if(currArg.at(j) == 'R') {
 					RFlag = 1;
 				}
 				// if user inputs a flag that is not a, l, or R
 				// output error and exit(1)
 				else {
-					cout << "Error: Flag(s) cannot be handled." << endl;
+					cout << "Error: One or more of these flags cannot be handled." << endl;
 					exit(1);
 				}
 			}
+		}
+		
+		// else currArg does not begin with '-', it is considered a file/directory name
+		// do the following
+		else {
+			
+			filesVec.push_back(currArg);
 		}
 	}
 
 	cout << "aFlag: " << aFlag << endl;
 	cout << "lFlag: " << lFlag << endl;
 	cout << "RFlag: " << RFlag << endl;
+
+	cout << "filesVec: ";
+	for(unsigned i = 0; i < filesVec.size(); i++) {
+		cout << '<' << filesVec.at(i) << "> ";
+	}
+	cout << endl;
 	
 	return 0;
 
