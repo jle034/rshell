98f4ca806e49edabb09e8407b391e2f2861a3962
Jennifer Le <jle034@hammer.cs.ucr.edu>
2015-05-01 00:54:09 -0700

    -l and -la flags work, including total

--- src/ls.cpp
+++ src/ls.cpp
@@ -5,12 +5,22 @@
 #include <iostream>
 #include <vector>
 #include <sys/stat.h>
+//#include <sys/types.h>
 #include <stdio.h>
 #include <algorithm>
+//#include <unistd.h>
+#include <pwd.h>
+#include <grp.h>
+#include <dirent.h>
+#include <string.h>
 
 using namespace std;
 
-void print(vector<string> fdVec, int aFlag, int lFlag, int RFlag);
+
+// helper functions :]
+void printSingleFile(struct stat s);
+void printEverything(vector<string> fdVec, int aFlag, int lFlag, int RFlag, int fdOriginalSize);
+void printl(struct stat s, vector<string>fdVec);
 
 int main(int argc, char* argv[]) {
 	
@@ -81,20 +91,178 @@ int main(int argc, char* argv[]) {
 
 	cout << "fdVec: ";
 
+	int fdOriginalSize = fdVec.size();
+
 	for(unsigned i = 0; i < fdVec.size(); i++) {
 		cout << '<' << fdVec.at(i) << "> ";
 	}
 	cout << endl;
 
-	for(unsigned i = 0; i < fdVec.size(); i++) {
-		print(fdVec, aFlag, lFlag, RFlag);
-	}
-	
+	while(fdVec.size()) {
+		printEverything(fdVec, aFlag, lFlag, RFlag, fdOriginalSize);
+		fdVec.erase(fdVec.begin());
+	}	
+
+	cout << endl;
+
 	return 0;
 }
 
-void print(vector<string> fdVec, int aFlag, int lFlag, int RFlag) {
+void printSingleFile(struct stat s) {
+
+}
+
+void printl(struct stat s, string fdName) {
+	
+	string normal = "\033[0;00m";
+	string blueOnGray = "\033[1;100;36m";
+	
+	// if link
+	// print 'l'
+	if(S_ISLNK(s.st_mode)) {
+		cout << 'l';
+	}
+	// else if directory
+	// print 'd'
+	else if(S_ISDIR(s.st_mode)) {
+		cout << 'd';
+	}
+	// else, neither link nor directory
+	// print '-'
+	else {
+		cout << '-';
+	}
+
+	// if user has read permission, print 'r'
+	// else print '-' indicating no read permission
+	//cout << (s.st_mode & S_IRUSR)?"r":"-"
+	if(s.st_mode & S_IRUSR) {
+		cout << 'r';
+	}
+	else {
+		cout << '-';
+	}
+	// if user has write permission, print 'w'
+	// else print '-' indicating no write permission
+	if(s.st_mode & S_IWUSR) {
+		cout << 'w';
+	}
+	else {
+		cout << '-';
+	}
+	// if user has execute permission, print 'x'
+	// else print '-' indicating no execute permission
+	if(s.st_mode & S_IXUSR) {
+		cout << 'x';
+	}
+	else {
+		cout << '-';
+	}
+
+	//----------------------------------------
+	// if group has read permission, print 'r'
+	// else print '-' indicating no read permission
+	if(s.st_mode & S_IRGRP) {
+		cout << 'r';
+	}
+	else {
+		cout << '-';
+	}
+	// if group has write permission, print 'w'
+	// else print '-' indicating no write permission
+	if(s.st_mode & S_IWGRP) {
+		cout << 'w';
+	}
+	else {
+		cout << '-';
+	}
+	// if group has execute permission, print 'x'
+	// else print '-' indicating no execute permission
+	if(s.st_mode & S_IXGRP) {
+		cout << 'x';
+	}
+	else {
+		cout << '-';
+	}
+
+	//----------------------------
+	// if others has read permission, print 'r'
+	// else print '-' indicating no read permission
+	if(s.st_mode & S_IROTH) {
+		cout << 'r';
+	}
+	else {
+		cout << '-';
+	}
+	// if others has write permission, print 'w'
+	// else print '-' indicating no write permission
+	if(s.st_mode & S_IWOTH) {
+		cout << 'w';
+	}
+	else {
+		cout << '-';
+	}
+	// if others has execute permission, print 'x'
+	// else print '-' indicating no execute permission
+	if(s.st_mode & S_IXOTH) {
+		cout << 'x';
+	}
+	else {
+		cout << '-';
+	}
+	
+	cout << ' ';
+
+	// print number of hard links
+	cout << s.st_nlink;
+
+	cout << ' ';
+
+	// print username
+	struct passwd *userid = getpwuid(s.st_uid);
+	if(userid == NULL) {
+		perror("getpwuid");
+		exit(1);
+	}
+	else {
+		cout << userid->pw_name;
+	}
+	
+	cout << ' ';
+	
+	// print groupname
+	struct group *groupid = getgrgid(s.st_gid);
+	if(groupid == NULL) {
+		perror("getgrgid");
+		exit(1);
+	}
+	else {
+		cout << groupid->gr_name;
+	}
+	
+	cout << ' ';
+
+	cout << s.st_size;
+
+	cout << ' ';
+
+	time_t time = s.st_mtime;
+	struct tm *thisTime = localtime(&time);
+	char printTime[100];
+	strftime(printTime, 100, "%b %e %H:%M", thisTime);
+	cout << printTime;
+
+	cout << ' ';
+
+	cout << fdName << endl;
+
+
+}
+
+void printEverything(vector<string> fdVec, int aFlag, int lFlag, int RFlag, int fdOriginalSize) {
 	struct stat s;
+
+	cout << "HERE" << endl;
 
 	// if error with stat
 	// perror("stat")
@@ -105,9 +273,108 @@ void print(vector<string> fdVec, int aFlag, int lFlag, int RFlag) {
 	// if regular file
 	// do the following
 	if(S_ISREG(s.st_mode)) {
+		
+		cout << "IS REGULAR FILE" << endl;
 
+		// if lFlag is included
+		// do the following
+		if(lFlag) {
+			printl(s, fdVec.at(0));
+		}
+		
 	}
+	else if(S_ISDIR(s.st_mode)) {
+
+		int total = 0;
+		vector<string> newDirEntVec;
+		
+		cout << "IS DIRECTORY" << endl;
 	
+		DIR *currDir = opendir((fdVec.at(0)).c_str());
+		if(currDir == NULL) {
+			perror("opendir");
+			exit(1);
+		}
+		
+		if(fdOriginalSize >1) {
+			cout << fdVec.at(0) << ":" << endl;
+		}
+		
+		dirent* currDirEnt;
+
+	/*
+		while((currDirEnt = readdir(currDir))) {
+			if(currDirEnt == NULL) {
+				perror("readdir");
+				exit(1);
+			}
+			char temp[512];
+			strcpy(temp, (fdVec.at(0)).c_str());
+			strcat(temp, "/");
+			strcat(temp, currDirEnt->d_name);
+
+			if(lstat(temp, &s) == -1) {
+				perror("stat");
+				exit(1);	
+			}
+
+			// if lFlag
+			// do the following
+			if(lFlag) {
+				// if aFlag
+				// printl like normal	
+				if(aFlag) {
+					printl(s, currDirEnt->d_name);
+				}	
+				// else no aFlag
+				// check for hidden files
+				// only print l on files nonhidden files
+				else if((currDirEnt->d_name[0] != '.')) {
+					printl(s, currDirEnt->d_name);
+				}
+			}	
+		}
+	*/
+		while((currDirEnt = readdir(currDir))) {
+			if(currDirEnt == NULL) {
+				perror("readdir");
+				exit(1);
+			}
+			char temp[512];
+			strcpy(temp, (fdVec.at(0)).c_str());
+			strcat(temp, "/");
+			strcat(temp, currDirEnt->d_name);
+
+			if(lstat(temp, &s) == -1) {
+				perror("stat");
+				exit(1);
+			}
+			
+			if(lFlag) {
+				if(aFlag) {
+					total += s.st_blocks;
+					newDirEntVec.push_back(temp);
+				}
+				else if((currDirEnt->d_name[0] != '.')) {
+					total += s.st_blocks;
+					newDirEntVec.push_back(temp);
+				}
+			}
+		}
+
+		sort (newDirEntVec.begin(), newDirEntVec.end(), locale("en_US.UTF-8"));
+
+		if(lFlag) {
+			cout << "total " << total/2 << endl;
+			for(unsigned i = 0; i < newDirEntVec.size(); i++) {
+				if(lstat(newDirEntVec.at(i).c_str(), &s) == -1) {
+					perror("lstat");
+				}
+				string temp;
+				printl(s, newDirEntVec.at(i));
+			}
+		}
+	}	
 }
 
 
