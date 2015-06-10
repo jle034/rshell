81dfffe77f1cc355f89f09a62c0272ca1d61e658
Jennifer Le <jle034@hammer.cs.ucr.edu>
2015-06-09 18:55:21 -0700

    added an ex2.cpp

--- grep/ex2.cpp
+++ grep/ex2.cpp
@@ -14,4 +14,24 @@ int main(int argc, char** argv) {
 	cerr << "fd[0]" << fd[0] << endl;
 	cerr << "fd[1]" << fd[1] << endl;
 
+    int pid=fork();
+    if (pid==0) {
+        close(1);
+        dup(fd[1]);
+        if (-1==write(fd[1],"hello world", 11)) {
+            perror ("write");
+        }
+        cout << "goodbye world" << endl;
+    }
+    else if (pid>0) {
+        close(0);
+        dup(fd[0]);
+        string buf;
+        cin >> buf;
+        //char buf[BUFSIZ];
+        //read(fd[0],buf,BUFSIZ);
+        cerr << "buf=" << buf << endl;
+    }
+    return 0;
+}
 
